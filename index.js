6a3a9f84af5814780e1df0ca46963271229c8cbc
Nicolas Cardelino <ncardeli@gmail.com>
2021-01-14 16:02:59 -0300

    Add test for send event and identity

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const btnSendPageView = document.querySelector("#btn-send-pageview");
 btnSendPageView.addEventListener("click", () =>
   gaProvider.page(location.href, "Testing analytics")
 );
+
+const btnSendEvent = document.querySelector("#btn-send-event");
+btnSendEvent.addEventListener("click", () =>
+  gaProvider.track("test-event", { value: 100 })
+);
+
+const btnSendIdentity = document.querySelector("#btn-send-identity");
+btnSendIdentity.addEventListener("click", () =>
+  gaProvider.identify("test-user-123")
+);
